6c0bd6cf247ffef6dae527b2a967958e8c8c8764
verosut <v_traverso@hotmail.com.ar>
2022-01-16 15:40:03 -0300

    primer proyecto

---
+++ ADA PRIMER PROYECTO/src/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+
+/**
+ * The HelloWorldApp class implements an application that simply prints "Hello
+ * World!" to standard output.
+ */
+
+public class HelloWorld {
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); // Display the string.
+	}
+}
